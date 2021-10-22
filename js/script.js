0568049fa99dcba15b79c6706cab578b9a304cd0
Avetisyan.ae <avetisyan.ae@formularf.ru>
2021-10-22 19:41:03 +0300

    clean layout

--- js/script.js
+++ js/script.js
@@ -1,47 +1,6 @@
 new WOW().init();
 
 $(document).ready(function () {
-
-    let widthWindow = $(window).width();
-
-    function toggleMenu() {
-        if ($(this).width() <= 800) {
-            $('footer .toggle_menu').each(function () {
-                $(this).find('.accordion').hide();
-                $('footer .toggle_menu').find('i.icon').removeClass('minus').addClass('plus')
-            })
-        } else {
-            $('footer .toggle_menu').each(function () {
-                $(this).find('.accordion').show();
-            })
-
-            $('footer .toggle_menu').click(function () {
-                event.stopPropagation()
-            })
-        }
-    }
-
-    $('footer .toggle_menu').click(function () {
-        if ( $(window).width() <= 800) {
-            $(this).toggleClass('active')
-            $(this).each( function () {
-                if ( $(this).hasClass('active') ) {
-                    $(this).find('.accordion').slideDown('fast');
-                    $(this).find('i.icon').removeClass('plus').addClass('minus')
-                } else {
-                    $(this).removeClass('active').find('.accordion').slideUp('fast');
-                    $(this).find('i.icon').removeClass('minus').addClass('plus')
-                }
-            })
-        }
-    })
-
-    toggleMenu()
-
-    $(window).on('resize', function () {
-        toggleMenu()
-    })
-
     $('a[href^="#"]').click(function(){
         let anchor = $(this).attr('href');
         $('html, body').animate({
