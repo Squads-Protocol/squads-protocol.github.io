de7954eeaae2066e9215e1e3b9eb0006a0fda0cb
Avetisyan.ae <avetisyan.ae@formularf.ru>
2021-10-25 15:34:38 +0300

    clean layout

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     $('a[href^="#"]').click(function(){
         let anchor = $(this).attr('href');
         $('html, body').animate({
-            scrollTop:  $(anchor).offset().top
+            scrollTop:  $(anchor).offset().top-50
         }, 600);
     });
 
