eeaf8e1813602e1cbc1299bfa304469a1ed937b1
Sean Lars Ganser <sean@sqds.io>
2022-02-23 01:44:38 -0500

    Revert "Merge pull request #2 from squads-dapp/new-landing"
    This reverts commit 5261e6d4d0c7037974a4976bf804c08916662e10, reversing
    changes made to e02100e28c3f1f06688d04fcca2468c4e1e17832.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+new WOW().init();
+
+$(document).ready(function () {
+    $('a[href^="#"]').click(function(){
+        let anchor = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop:  $(anchor).offset().top-50
+        }, 600);
+    });
+
+})
+
+
+
+
