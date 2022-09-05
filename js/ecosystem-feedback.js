5a0819c5b22b3fc228dd6e272fd0f644cae13737
gorbatenko.mp <hertam1@2>
2022-09-05 10:42:52 +0300

    update

--- js/ecosystem-feedback.js
+++ js/ecosystem-feedback.js
@@ -2,18 +2,19 @@ $(document).ready(function () {
     var splideNews = new Splide( '.splide-feedback', {
         type   : 'loop',
         pagination: false,
-        autoWidth: true,
         perMove: 1,
+        perPage: 6,
+        autoWidth: true,
         autoScroll: {
             speed: 1.5,
         },
         breakpoints: {
-            0: {
-                perPage: 1,
-            },
             767: {
                 perPage: 1,
-                autoWidth: false,
+                autoWidth: true,
+                autoScroll: {
+                    speed: 1,
+                },
             }
         },
         classes: {
